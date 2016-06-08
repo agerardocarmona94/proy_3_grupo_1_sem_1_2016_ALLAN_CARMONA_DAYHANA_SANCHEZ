4ea122ebac081ee5368f0332781514033935da81
Dayhana <dayhana.sj@gmail.com>
2016-06-07 21:05:01 -0600

    Documentado

--- clk_div.v
+++ clk_div.v
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
-
+// Divisor utilizado para poder trabajar a una señal de reloj de 25MHz, el divisor solo convierte de 100 a 50MHz, xq en el módulo del VGA 
+//ya se encuentra otro divisor, que convierte de 50 a 25MHz
 module clk_div(clk ,out_clk);
 output reg out_clk;
 input clk ;
