4eb6d25454f844b18793e3906cff50cede795364
Allan Gerardo Carmona Cruz <agerardocarmona94@gmail.com>
2016-06-07 21:20:23 -0600

    Cambio final del modulo

--- TRI_STATE.v
+++ TRI_STATE.v
@@ -1,29 +1,12 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    00:06:29 04/09/2016 
-// Design Name: 
-// Module Name:    Buffer_Triestado_Salida 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+// en este modulo se activa el triestado a la salida de la FPGA para no enviar datos al RTC y se puedan leer datos del mismo
+
 module triestado_salida(
-    input [7:0] Deco_out,
+    input [7:0] Deco_out,     // entrada del tri estado
     input en,
-    output [7:0] RTC_in
+    output [7:0] RTC_in   //Salida del triestado
     );
 	 
-	 assign RTC_in = en ? Deco_out : 8'bz; 
+	 assign RTC_in = en ? Deco_out : 8'bz; //Operacion del Triestado, si tiene el enable activo pone el dato, sino alta impedancia
 		 
 endmodule
