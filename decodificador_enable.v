360fff3311edfb7dff3443264736afaa4820126d
Allan Gerardo Carmona Cruz <agerardocarmona94@gmail.com>
2016-06-07 21:33:00 -0600

    cambios finales

--- decodificador_enable.v
+++ decodificador_enable.v
@@ -1,25 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    17:37:26 05/31/2016 
-// Design Name: 
-// Module Name:    decodificador_enable 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+//-----------------------------------------------------------------
+/*En este modulo se decodifica la activcion de los enable de cada registro que van al VGA
+*/
+//-------------------------------------------------------------------
 module decodificador_enable(
-input wire [7:0] direccion,
+input wire [7:0] direccion,  //la entrada corresponde a la direccion que va recorriendo el picoblaze en las secuencias de lectura y escritura
 input reset,clk,
 output reg [8:0]enable 
     );
@@ -30,7 +15,7 @@ output reg [8:0]enable
       else
          case (direccion)
             8'b0000  : enable <= 9'b000000001;
-            8'b0001  : enable <= 9'b000000010;
+            8'b0001  : enable <= 9'b000000010;   //como se observa hay un bit para cada enable en el decodificador
             8'b0010  : enable <= 9'b000000100;
             8'b0011  : enable <= 9'b000001000;
             8'b0100  : enable <= 9'b000010000;
